9842734dc9de3b5ec7165d2bf4c168fb157e0111
ko-ing <318921@naver.com>
2022-03-01 14:13:15 +0900

    로컬 db 연결

--- main.go
+++ main.go
@@ -1,8 +1,31 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"time"
+)
+
+type Emoji struct {
+	gorm.Model
+	Id        string
+	Name      string
+	UserId    string
+	ImageUrl  string
+	CreatedAt time.Time
+}
 
 func main() {
+	dsn := "root:lostfinder123@tcp(127.0.0.1:3306)/jjal_talk?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		panic("Connecting Database Failed")
+	}
+
+	db.AutoMigrate(&Emoji{})
+
 	router := gin.Default()
 
 	router.GET("/", func(c *gin.Context) {
@@ -17,5 +40,5 @@ func main() {
 		})
 	})
 
-	router.Run(":5000")
+	router.Run(":8000")
 }
